2ec5e2b5a4e50fd9f49f3d9635a79724dbc52ee8
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-11-18 22:38:40 +0100

    Adding cards

--- lib/main.dart
+++ lib/main.dart
@@ -13,36 +13,88 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage(
-                    "images/profile.png",
-                  ),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage(
+                  "images/profile.png",
                 ),
-                Text(
-                  'Rune Bjørsnøs',
-                  style: TextStyle(
-                    fontSize: 32,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Pacifico',
-                  ),
+              ),
+              Text(
+                'Rune Bjørsnøs',
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
                 ),
-                Text(
-                  'WEB DEVELOPER',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white60,
-                    fontFamily: 'SourceSansPro',
-                    letterSpacing: 4,
-                  ),
+              ),
+              Text(
+                'WEB DEVELOPER',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white60,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 4,
                 ),
-              ],
-            ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "+47 988 52 148",
+                      style: TextStyle(
+                        fontFamily: "SourceSansPro",
+                        fontSize: 20,
+                        color: Colors.teal.shade900,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "rune.bjorsnos@gmail.com",
+                      style: TextStyle(
+                        fontFamily: "SourceSansPro",
+                        fontSize: 20,
+                        color: Colors.teal.shade900,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
